d67e36a732a292d5c69acdf2d0a74283004de196
Noyan Çepikkurt <cepikkurtnoyan@hotmail.com>
2022-10-12 22:18:38 +0300

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Noyan Çepikkurt on 12.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
